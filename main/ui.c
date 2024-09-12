0645610cdf3ee6a7e998f7bd70fc85ac61ae67b9
giobauermeister <giovanni.bauermeister@gmail.com>
2024-09-12 19:57:53 -0300

    add ui elements

--- main/ui.c
+++ main/ui.c
@@ -34,14 +34,14 @@ void anim_border_opacity_cb(void *rect_obj, int32_t opacity) {
 void create_row_rectangles(lv_obj_t *parent, int16_t n_rect) {
     int16_t rect_width = 70;
     int16_t rect_height = 100;
-    int16_t spacing = 20;
+    int16_t spacing = 14;
     lv_obj_t *previous_rect = NULL;
 
     // Create style for rectangles
     static lv_style_t rect_style;
     lv_style_init(&rect_style);
     lv_style_set_border_color(&rect_style, lv_color_black()); // Set border color to black
-    lv_style_set_border_width(&rect_style, 4);                // Border width of 4 pixels
+    lv_style_set_border_width(&rect_style, 3);                // Border width of 4 pixels
     lv_style_set_radius(&rect_style, 5);                      // Set border radius to 5
     lv_style_set_border_opa(&rect_style, LV_OPA_COVER);       // Set border opacity to fully opaque
     lv_style_set_bg_opa(&rect_style, LV_OPA_TRANSP);          // Set background opacity to transparent
@@ -52,7 +52,7 @@ void create_row_rectangles(lv_obj_t *parent, int16_t n_rect) {
         lv_obj_set_size(rectangles[i], rect_width, rect_height);
 
         if(i == 0) {
-            lv_obj_align(rectangles[i], LV_ALIGN_LEFT_MID, 100, 0); // Position on the screen
+            lv_obj_align(rectangles[i], LV_ALIGN_LEFT_MID, 110, 0); // Position on the screen
         } else {
             lv_obj_align_to(rectangles[i], previous_rect, LV_ALIGN_OUT_RIGHT_MID, spacing, 0);
         }
@@ -100,21 +100,68 @@ void create_ui(void) {
     // lv_label_set_text(label, "Click Me");
     // lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
-    // // // Create a style and set a larger font size
-    // static lv_style_t style;
-    // lv_style_init(&style);
-    // lv_style_set_text_font(&style, &lv_font_montserrat_30);  // Use a larger built-in font
+    // Create a static label
+    lv_obj_t *ui_lb_your_vote_for = lv_label_create(screen);
+    lv_label_set_text(ui_lb_your_vote_for, "SEU VOTO PARA");
+    lv_obj_set_style_text_font(ui_lb_your_vote_for, &lv_font_montserrat_20, LV_PART_MAIN);
+    lv_obj_align(ui_lb_your_vote_for, LV_ALIGN_TOP_LEFT, 10, 15);
 
-    // // Create a static label
-    // lv_obj_t *txt = lv_label_create(screen);
-    // lv_label_set_text(txt, "Hello World!");
-    // lv_obj_align(txt, LV_ALIGN_CENTER, 0, 0);
-    // lv_obj_add_style(txt, &style, 0);
+    lv_obj_t *lb_candidate_role = lv_label_create(screen);
+    lv_label_set_text(lb_candidate_role, "Prefeito");
+    lv_obj_set_style_text_font(lb_candidate_role, &lv_font_montserrat_44, LV_PART_MAIN);
+    lv_obj_align(lb_candidate_role, LV_ALIGN_TOP_LEFT, 150, 100);
+
+    lv_obj_t *ui_lb_number = lv_label_create(screen);
+    lv_label_set_text(ui_lb_number, "Numero:");
+    lv_obj_set_style_text_font(ui_lb_number, &lv_font_montserrat_22, LV_PART_MAIN);
+    lv_obj_align(ui_lb_number, LV_ALIGN_LEFT_MID, 5, 0);
+
+    lv_obj_t *ui_lb_name = lv_label_create(screen);
+    lv_label_set_text(ui_lb_name, "Nome:");
+    lv_obj_set_style_text_font(ui_lb_name, &lv_font_montserrat_22, LV_PART_MAIN);
+    lv_obj_align(ui_lb_name, LV_ALIGN_TOP_LEFT, 5, 315);
+
+    lv_obj_t *lb_cadidate_name = lv_label_create(screen);
+    lv_label_set_text(lb_cadidate_name, "Giovanni B Santana");
+    lv_obj_set_style_text_font(lb_cadidate_name, &lv_font_montserrat_26, LV_PART_MAIN);
+    lv_obj_align_to(lb_cadidate_name, ui_lb_name, LV_ALIGN_OUT_RIGHT_MID, 45, 0);
+
+    lv_obj_t *ui_lb_party = lv_label_create(screen);
+    lv_label_set_text(ui_lb_party, "Partido:");
+    lv_obj_set_style_text_font(ui_lb_party, &lv_font_montserrat_22, LV_PART_MAIN);
+    lv_obj_align(ui_lb_party, LV_ALIGN_TOP_LEFT, 5, 355);
+
+    lv_obj_t *lb_party = lv_label_create(screen);
+    lv_label_set_text(lb_party, "Exemplo");
+    lv_obj_set_style_text_font(lb_party, &lv_font_montserrat_26, LV_PART_MAIN);
+    lv_obj_align_to(lb_party, ui_lb_party, LV_ALIGN_OUT_RIGHT_MID, 30, 0);
 
     create_row_rectangles(screen, NUM_RECTANGLES);
 
     blink_rectangle(0, true);
     blink_rectangle(1, false);
-    blink_rectangle(2, true);
+    blink_rectangle(2, false);
     blink_rectangle(3, false);
+
+    // Create line at the bottom
+    lv_obj_t *ui_bottom_line = lv_obj_create(screen);
+    lv_obj_set_size(ui_bottom_line, 800, 3);
+    lv_obj_set_pos(ui_bottom_line, 0, 410);
+    lv_obj_set_style_border_color(ui_bottom_line, lv_color_black(), LV_PART_MAIN);
+
+    lv_obj_t *ui_lb_press_key = lv_label_create(screen);
+    lv_label_set_text(ui_lb_press_key, "Aperte a tecla:");
+    lv_obj_set_style_text_font(ui_lb_press_key, &lv_font_montserrat_20, LV_PART_MAIN);
+    lv_obj_align(ui_lb_press_key, LV_ALIGN_TOP_LEFT, 5, 415);
+
+    lv_obj_t *ui_lb_confirm = lv_label_create(screen);
+    lv_label_set_text(ui_lb_confirm, "CONFIRMA para PROSEGUIR");
+    lv_obj_set_style_text_font(ui_lb_confirm, &lv_font_montserrat_20, LV_PART_MAIN);
+    lv_obj_align(ui_lb_confirm, LV_ALIGN_TOP_LEFT, 25, 435);
+
+    lv_obj_t *ui_lb_restart = lv_label_create(screen);
+    lv_label_set_text(ui_lb_restart, "CORRIGE para REINICIAR este voto");
+    lv_obj_set_style_text_font(ui_lb_restart, &lv_font_montserrat_20, LV_PART_MAIN);
+    lv_obj_align(ui_lb_restart, LV_ALIGN_TOP_LEFT, 45, 455);
+
 }
