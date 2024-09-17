bce4cdd4430a2a89c8886720418196cb214ec1c7
Giovanni Bauermeister <giovanni.bauermeister@gmail.com>
2024-09-17 07:58:21 -0300

    remove commented code

--- main/ui.c
+++ main/ui.c
@@ -120,20 +120,9 @@ void update_ui_keypress(char key)
 void create_ui(void) {
 
     lv_obj_t *screen = lv_screen_active();
-    // lv_obj_clear_flag(screen, LV_OBJ_FLAG_SCROLLABLE);
 
     // Set screen background to white
     set_screen_background_white(screen);
-
-    // // Create a button
-    // lv_obj_t *btn = lv_button_create(screen);
-    // lv_obj_set_size(btn, 120, 50);  // Set button size
-    // lv_obj_set_pos(btn, 50, 50);    // Position the button
-
-    // // Add a label to the button
-    // lv_obj_t *label = lv_label_create(btn);
-    // lv_label_set_text(label, "Click Me");
-    // lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
     // Create a static label
     lv_obj_t *ui_lb_your_vote_for = lv_label_create(screen);
