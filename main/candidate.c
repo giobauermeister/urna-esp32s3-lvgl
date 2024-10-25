34111b67c627a3fc8d457ee344a437836e43284c
Giovanni Bauermeister <giovanni.bauermeister@gmail.com>
2024-10-24 22:14:47 -0300

    add defines for file names; implement function get_party_by_id; add function get_party_by_id to search_candidate so party name string can display on screen

--- main/candidate.c
+++ main/candidate.c
@@ -7,9 +7,10 @@
 
 static const char* TAG_ADD_CANDIDATE = "Candidate[ADD]";
 static const char* TAG_DEL_CANDIDATE = "Candidate[DEL]";
-static const char* SEARCH_CANDIDATE = "Candidate[SEARCH]";
+static const char* TAG_SEARCH_CANDIDATE = "Candidate[SEARCH]";
 static const char* TAG_ADD_PARTY = "Party[ADD]";
 static const char* TAG_DEL_PARTY = "Party[DEL]";
+static const char* TAG_GET_PARTY = "Party[GET]";
 static const char* TAG_CHECK_PARTY = "Party[CHECK]";
 static const char* TAG_ADD_ROLE = "Role[ADD]";
 static const char* TAG_DEL_ROLE = "Role[DEL]";
@@ -32,7 +33,7 @@ esp_err_t add_candidate(Candidate new_candidate)
 
     int current_id = 1;
 
-    FILE *f = fopen("/sd/candidates.jdb", "r+");
+    FILE *f = fopen(FILE_CANDIDATES, "r+");
     if(f != NULL) {
         // File exists, read and find last registered id
         char line[256];
@@ -58,7 +59,7 @@ esp_err_t add_candidate(Candidate new_candidate)
         current_id++;
     } else {
         // File does not exist, create new file and use id = 1
-        f = fopen("/sd/candidates.jdb", "w");
+        f = fopen(FILE_CANDIDATES, "w");
         if(f == NULL) {
             ESP_LOGE(TAG_ADD_CANDIDATE, "Failed to open file candidates.jdb for writing");
             return ESP_FAIL;
@@ -92,21 +93,21 @@ esp_err_t del_candidate_by_id(int candidate_id)
 {
     ESP_LOGI(TAG_DEL_CANDIDATE, "Delete candidate by id");
 
-    FILE *f = fopen("/sd/candidates.jdb", "r");
+    FILE *f = fopen(FILE_CANDIDATES, "r");
     if(f == NULL) {
-        ESP_LOGE(TAG_DEL_CANDIDATE, "Could not open file candidates.jdb for reading");
+        ESP_LOGE(TAG_DEL_CANDIDATE, "Could not open file %s for reading", FILE_CANDIDATES);
         return ESP_FAIL;
     }
 
-    FILE *f_temp = fopen("/sd/temp.jdb", "w");
+    FILE *f_temp = fopen(FILE_TEMP, "w");
     if(f_temp == NULL) {
-        ESP_LOGE(TAG_DEL_CANDIDATE, "Could not open file temp.jdb for reading");
+        ESP_LOGE(TAG_DEL_CANDIDATE, "Could not open file %s for reading", FILE_TEMP);
         fclose(f);
         return ESP_FAIL;
     }
 
     char line[256];
-    bool candidate_found = false;
+    bool flag_candidate_found = false;
 
     while(fgets(line, sizeof(line), f)) {
         cJSON *candidate_obj = cJSON_Parse(line);
@@ -126,7 +127,7 @@ esp_err_t del_candidate_by_id(int candidate_id)
         int id = id_item->valueint;
         if(id == candidate_id) {
             ESP_LOGI(TAG_DEL_CANDIDATE, "Candidate with id %d found and deleted", candidate_id);
-            candidate_found = true;
+            flag_candidate_found = true;
             // Do not write this line to the temp file, effectively deleting it
         } else {
             // Write the line to the temp file if it doesn't match the candidate ID
@@ -139,33 +140,36 @@ esp_err_t del_candidate_by_id(int candidate_id)
     fclose(f);
     fclose(f_temp);
 
-    if(!candidate_found) {
+    if(!flag_candidate_found) {
         ESP_LOGE(TAG_DEL_CANDIDATE, "Candidate with id %d not found", candidate_id);
-        remove("/sd/temp.jdb");  // Delete the temporary file since no changes were made
+        remove(FILE_TEMP);  // Delete the temporary file since no changes were made
         return ESP_ERR_NOT_FOUND;
     }
 
     // Replace the original file with the updated temporary file
-    remove("/sd/candidates.jdb");  // Delete the original file
-    rename("/sd/temp.jdb", "/sd/candidates.jdb");  // Rename temp file to original file
+    remove(FILE_CANDIDATES);  // Delete the original file
+    rename(FILE_TEMP, FILE_CANDIDATES);  // Rename temp file to original file
 
     ESP_LOGI(TAG_DEL_CANDIDATE, "Candidate with id %d deleted successfully", candidate_id);
 
     return ESP_OK;
 }
 
-esp_err_t search_candidate(const char* candidate_number, Candidate* found_candidate) {
-    ESP_LOGI(SEARCH_CANDIDATE, "Check if candidate exists: %s", candidate_number);
+esp_err_t search_candidate(const char* candidate_number, Candidate* found_candidate)
+{
+    ESP_LOGI(TAG_SEARCH_CANDIDATE, "Search candidate by number: %s", candidate_number);
 
     memset(found_candidate, 0, sizeof(Candidate));
 
-    FILE *f = fopen("/sd/candidates.jdb", "r");
+    FILE *f = fopen(FILE_CANDIDATES, "r");
     if(f == NULL) {
-        ESP_LOGE(SEARCH_CANDIDATE, "Could not open file candidates.jdb for reading");
+        ESP_LOGE(TAG_SEARCH_CANDIDATE, "Could not open file %s for reading", FILE_CANDIDATES);
         return ESP_FAIL;
     }
 
     char line[256];
+    Party found_party;
+
     while (fgets(line, sizeof(line), f))
     {
         cJSON *candidate_obj = cJSON_Parse(line);
@@ -176,9 +180,15 @@ esp_err_t search_candidate(const char* candidate_number, Candidate* found_candid
                 found_candidate->id = cJSON_GetObjectItem(candidate_obj, "id")->valueint;
                 found_candidate->name = strdup(cJSON_GetObjectItem(candidate_obj, "name")->valuestring);
                 found_candidate->number = strdup(cJSON_GetObjectItem(candidate_obj, "number")->valuestring);
-                found_candidate->role_id = cJSON_GetObjectItem(candidate_obj, "role")->valueint;
+                found_candidate->role_id = cJSON_GetObjectItem(candidate_obj, "role_id")->valueint;
                 found_candidate->party_id = cJSON_GetObjectItem(candidate_obj, "party_id")->valueint;
-                // found_candidate->photo_path = strdup(cJSON_GetObjectItem(candidate_obj, "photo_path")->valuestring);
+
+                // Get party by id
+                if(get_party_by_id(found_candidate->party_id, &found_party) == ESP_OK) {
+                    found_candidate->party_name = found_party.name;
+                } else {
+                    found_candidate->party_name = strdup("Sem partido");
+                }
 
                 cJSON_Delete(candidate_obj);
                 fclose(f);
@@ -186,7 +196,7 @@ esp_err_t search_candidate(const char* candidate_number, Candidate* found_candid
             }
             cJSON_Delete(candidate_obj);
         } else {
-            ESP_LOGE(SEARCH_CANDIDATE, "Failed to parse JSON line");
+            ESP_LOGE(TAG_SEARCH_CANDIDATE, "Failed to parse JSON line");
         }
     }
     
@@ -195,12 +205,11 @@ esp_err_t search_candidate(const char* candidate_number, Candidate* found_candid
 }
 
 // Helper function to clear candidate data (free memory)
-void clear_candidate(Candidate* candidate) {
+void free_candidate(Candidate* candidate) {
     if (candidate->name) free(candidate->name);
     if (candidate->number) free(candidate->number);
     if (candidate->role_name) free(candidate->role_name);
     if (candidate->party_name) free(candidate->party_name);
-    if (candidate->photo_path) free(candidate->photo_path);
     memset(candidate, 0, sizeof(Candidate));
 }
 
@@ -210,7 +219,7 @@ esp_err_t add_party(Party new_party)
 
     int current_id = 1;
 
-    FILE *f = fopen("/sd/parties.jdb", "r+");
+    FILE *f = fopen(FILE_PARTIES, "r+");
     if(f != NULL) {
         //File exists, read and find last registered id
         char line[256];
@@ -236,7 +245,7 @@ esp_err_t add_party(Party new_party)
         current_id++;
     } else {
         // File does not exist, create new file and use id = 1
-        f = fopen("/sd/parties.jdb", "w");
+        f = fopen(FILE_PARTIES, "w");
         if(f == NULL) {
             ESP_LOGE(TAG_ADD_PARTY, "Failed to open file parties.jdb for writing");
             return ESP_FAIL;
@@ -267,21 +276,21 @@ esp_err_t del_party_by_id(int party_id)
 {
     ESP_LOGI(TAG_DEL_PARTY, "Delete party by id");
     
-    FILE *f = fopen("/sd/parties.jdb", "r");
+    FILE *f = fopen(FILE_PARTIES, "r");
     if(f == NULL) {
-        ESP_LOGE(TAG_DEL_PARTY, "Could not open file parties.jdb for reading");
+        ESP_LOGE(TAG_DEL_PARTY, "Could not open file %s for reading", FILE_PARTIES);
         return ESP_FAIL;
     }
 
-    FILE *f_temp = fopen("/sd/temp.jdb", "w");
+    FILE *f_temp = fopen(FILE_TEMP, "w");
     if(f_temp == NULL) {
-        ESP_LOGE(TAG_DEL_PARTY, "Could not open file temp.jdb for reading");
+        ESP_LOGE(TAG_DEL_PARTY, "Could not open file %s for reading", FILE_TEMP);
         fclose(f);
         return ESP_FAIL;
     }
 
     char line[256];
-    bool party_found = false;
+    bool flag_party_found = false;
 
     while(fgets(line, sizeof(line), f)) {
         cJSON *party_obj = cJSON_Parse(line);
@@ -301,7 +310,7 @@ esp_err_t del_party_by_id(int party_id)
         int id = id_item->valueint;
         if(id == party_id) {
             ESP_LOGI(TAG_DEL_PARTY, "Party with id %d found and deleted", party_id);
-            party_found = true;
+            flag_party_found = true;
             // Do not write this line to the temp file, effectively deleting it
         } else {
             // Write the line to the temp file if it doesn't match the candidate ID
@@ -314,19 +323,71 @@ esp_err_t del_party_by_id(int party_id)
     fclose(f);
     fclose(f_temp);
 
-    if(!party_found) {
+    if(!flag_party_found) {
         ESP_LOGE(TAG_DEL_PARTY, "Party with id %d not found", party_id);
-        remove("/sd/temp.jdb"); // Delete the temporary file since no changes were made
+        remove(FILE_TEMP); // Delete the temporary file since no changes were made
         return ESP_ERR_NOT_FOUND;
     }
 
     // Replace the original file with the updated temporary file
-    remove("/sd/parties.jdb");
-    rename("/sd/temp.jdb", "/sd/parties.jdb");
+    remove(FILE_PARTIES);
+    rename(FILE_TEMP, FILE_PARTIES);
 
     ESP_LOGI(TAG_DEL_PARTY, "Party with id %d deleted successfully", party_id);
 
     return ESP_OK;
+}
+
+esp_err_t get_party_by_id(int party_id, Party* found_party)
+{
+    ESP_LOGI(TAG_GET_PARTY, "Get party by id: %d", party_id);
+
+    FILE *f = fopen(FILE_PARTIES, "r");
+    if(f == NULL) {
+        ESP_LOGE(TAG_GET_PARTY, "Could not open file %s for reading", FILE_PARTIES);
+        return ESP_FAIL;
+    }
+
+    char line[256];
+    cJSON *party_obj = NULL;
+
+    while(fgets(line, sizeof(line), f)) {
+        party_obj = cJSON_Parse(line);
+        if(party_obj) {
+            // Get party's ID from JSON object
+            cJSON *id_item = cJSON_GetObjectItem(party_obj, "id");
+            if(!cJSON_IsNumber(id_item)) {
+                ESP_LOGE(TAG_GET_PARTY, "Invalid or missing 'id' field");
+                cJSON_Delete(party_obj);
+                continue; // Skip lines without a valid ID
+            }
+
+            int id = id_item->valueint;
+            if(id == party_id) {
+                // Party is found, fill name element
+                cJSON *name_item = cJSON_GetObjectItem(party_obj, "name");
+
+                if(!name_item || !cJSON_IsString(name_item)) {
+                    ESP_LOGE(TAG_GET_PARTY, "Invalid or missing 'name' field");
+                    cJSON_Delete(party_obj);
+                    fclose(f);
+                    return ESP_ERR_NOT_FOUND;
+                }
+
+                ESP_LOGI(TAG_GET_PARTY, "Party with id %d found", party_id);
+                found_party->name = strdup(name_item->valuestring);
+                cJSON_Delete(party_obj);
+                fclose(f);
+                return ESP_OK;
+            }
+        } else {
+            ESP_LOGE(TAG_GET_PARTY, "Failed to parse JSON line");
+            continue;
+        }        
+    }
+
+    fclose(f);
+    return ESP_ERR_NOT_FOUND; // Party not found
 }
 
 esp_err_t add_role(Role new_role)
@@ -335,7 +396,7 @@ esp_err_t add_role(Role new_role)
 
     int current_id = 1;
 
-    FILE *f = fopen("/sd/roles.jdb", "r+");
+    FILE *f = fopen(FILE_ROLES, "r+");
     if(f != NULL) {
         //File exists, read and find last registered id
         char line[256];
@@ -361,7 +422,7 @@ esp_err_t add_role(Role new_role)
         current_id++;
     } else {
         // File does not exist, create new file and use id = 1
-        f = fopen("/sd/roles.jdb", "w");
+        f = fopen(FILE_ROLES, "w");
         if(f == NULL) {
             ESP_LOGE(TAG_ADD_ROLE, "Failed to open file parties.jdb for writing");
             return ESP_FAIL;
@@ -371,6 +432,7 @@ esp_err_t add_role(Role new_role)
     cJSON *role_obj = cJSON_CreateObject();
     cJSON_AddNumberToObject(role_obj, "id", current_id);
     cJSON_AddStringToObject(role_obj, "name", new_role.name);
+    cJSON_AddNumberToObject(role_obj, "n_digits", new_role.n_digits);
 
     char* rendered_json = cJSON_PrintUnformatted(role_obj);
     ESP_LOGI(TAG_ADD_ROLE, "%s", rendered_json);
@@ -392,15 +454,15 @@ esp_err_t del_role_by_id(int role_id)
 {
     ESP_LOGI(TAG_DEL_ROLE, "Delete role by id");
     
-    FILE *f = fopen("/sd/roles.jdb", "r");
+    FILE *f = fopen(FILE_ROLES, "r");
     if(f == NULL) {
-        ESP_LOGE(TAG_DEL_ROLE, "Could not open file roles.jdb for reading");
+        ESP_LOGE(TAG_DEL_ROLE, "Could not open file %s for reading", FILE_ROLES);
         return ESP_FAIL;
     }
 
-    FILE *f_temp = fopen("/sd/temp.jdb", "w");
+    FILE *f_temp = fopen(FILE_TEMP, "w");
     if(f_temp == NULL) {
-        ESP_LOGE(TAG_DEL_ROLE, "Could not open file temp.jdb for reading");
+        ESP_LOGE(TAG_DEL_ROLE, "Could not open file %s for reading", FILE_TEMP);
         fclose(f);
         return ESP_FAIL;
     }
@@ -441,13 +503,13 @@ esp_err_t del_role_by_id(int role_id)
 
     if(!role_found) {
         ESP_LOGE(TAG_DEL_ROLE, "Role with id %d not found", role_id);
-        remove("/sd/temp.jdb"); // Delete the temporary file since no changes were made
+        remove(FILE_TEMP); // Delete the temporary file since no changes were made
         return ESP_ERR_NOT_FOUND;
     }
 
     // Replace the original file with the updated temporary file
-    remove("/sd/roles.jdb");
-    rename("/sd/temp.jdb", "/sd/roles.jdb");
+    remove(FILE_ROLES);
+    rename(FILE_TEMP, FILE_ROLES);
 
     ESP_LOGI(TAG_DEL_ROLE, "Role with id %d deleted successfully", role_id);
 
@@ -458,9 +520,9 @@ esp_err_t check_party_exists(int party_id)
 {
     ESP_LOGI(TAG_CHECK_PARTY, "Check if party with %d exists", party_id);
 
-    FILE *f = fopen("/sd/parties.jdb", "r");
+    FILE *f = fopen(FILE_PARTIES, "r");
     if(f == NULL) {
-        ESP_LOGE(TAG_CHECK_PARTY, "Could not open file parties.jdb for reading");
+        ESP_LOGE(TAG_CHECK_PARTY, "Could not open file %s for reading", FILE_PARTIES);
         return ESP_FAIL;
     }
 
@@ -492,9 +554,9 @@ esp_err_t check_role_exists(int role_id)
 {
     ESP_LOGI(TAG_CHECK_ROLE, "Check if role with id %d exists", role_id);
 
-    FILE *f = fopen("/sd/roles.jdb", "r");
+    FILE *f = fopen(FILE_ROLES, "r");
     if(f == NULL) {
-        ESP_LOGE(TAG_CHECK_ROLE, "Could not open file roles.jdb for reading");
+        ESP_LOGE(TAG_CHECK_ROLE, "Could not open file %s for reading", FILE_ROLES);
         return ESP_FAIL;
     }
 
--- main/candidate.h
+++ main/candidate.h
@@ -3,6 +3,11 @@
 
 #include "esp_err.h"
 
+#define FILE_CANDIDATES "/sd/candidates.jdb"
+#define FILE_PARTIES    "/sd/parties.jdb"
+#define FILE_ROLES      "/sd/roles.jdb"
+#define FILE_TEMP       "/sd/temp.jdb"
+
 typedef struct {
     int id;
     char* name;
@@ -11,27 +16,28 @@ typedef struct {
     char* role_name;
     int party_id;
     char* party_name;
-    char* photo_path;
 } Candidate;
 
 typedef struct {
     int id;
-    const char* name;
+    char* name;
 } Party;
 
 typedef struct {
     int id;
-    const char* name;
+    char* name;
+    int n_digits;
 } Role;
 
 esp_err_t del_file_content(const char *file_path);
 esp_err_t add_candidate(Candidate new_dandidate);
 esp_err_t del_candidate_by_id(int candidate_id);
 esp_err_t search_candidate(const char* candidate_number, Candidate* found_candidate);
-void clear_candidate(Candidate* candidate);
+void free_candidate(Candidate* candidate);
 esp_err_t add_party(Party new_party);
 esp_err_t del_party_by_id(int party_id);
 esp_err_t check_party_exists(int party_id);
+esp_err_t get_party_by_id(int party_id, Party* found_party);
 esp_err_t add_role(Role new_role);
 esp_err_t del_role_by_id(int role_id);
 esp_err_t check_role_exists(int role_id);
